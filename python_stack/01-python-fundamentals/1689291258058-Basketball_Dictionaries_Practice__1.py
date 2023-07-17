454037e637334eedff494083381354bc919f2d72
Abedmalek daoud <133236356+malek774@users.noreply.github.com>
2023-07-17 17:58:22 +0100

    Add files via upload

---
+++ python_stack/01-python-fundamentals/1689291258058-Basketball_Dictionaries_Practice__1.py
@@ -0,0 +1,86 @@
+
+#! players Dictionary
+
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33,
+        "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32,
+        "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+
+#! Constructor 
+
+class Player:
+    def __init__(self, single_players):
+        self.name = single_players.name
+        self.age = single_players.age
+        self.position = single_players.position
+        self.team = single_players.team
+        
+# instance
+
+#! Question1
+xx = Player(players[0])
+print
+
+
+#! Question2
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+    
+player_jason = Player(jason)
+
+
+#! question 3
+new_team = []
+for x in players:
+    new_team.append(Player(x))
