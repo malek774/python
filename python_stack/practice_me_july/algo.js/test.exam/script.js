54b4d3e6ef6c18d6f91a59e449832efc9df771da
Abedmalek daoud <133236356+malek774@users.noreply.github.com>
2023-07-24 17:48:21 +0100

    Add files via upload

---
+++ python_stack/practice_me_july/algo.js/test.exam/script.js
@@ -0,0 +1,13 @@
+function alertt(){
+  alert("contact us at 97-455-655")
+}
+
+function add(){
+  document.querySelector(".number").innerText++
+}
+
+
+function sort(el){
+  var text = document.querySelector(".sp")
+  text.innerText = el.value
+}
